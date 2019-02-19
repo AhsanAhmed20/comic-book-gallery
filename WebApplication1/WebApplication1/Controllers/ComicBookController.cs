99b1f01ba294aeef330dcb4e41d267f2a388afc9
ahsanjutt20 <ahsanjutt20@hotmail.com>
2019-02-19 21:53:48 +0500

    Updated Details action method to  use  action result type

--- WebApplication1/WebApplication1/Controllers/ComicBookController.cs
+++ WebApplication1/WebApplication1/Controllers/ComicBookController.cs
@@ -8,9 +8,15 @@ namespace WebApplication1.Controllers
 {
     public class ComicBookController :Controller
     {
-       public string Detail()
-        {
-            return "Hello from the comic books controller!";
-        }
+           public ActionResult Detail()
+            {
+                if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the comic book Controller!");
+
+            }
     }
 }
